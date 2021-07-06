33235b2fc315c7e781dcd9e0bf0fc0a16760034d
Koichi Taura <taura.koichi@gmail.com>
2021-07-07 08:05:34 +0900

    add application settings

---
+++ src/main/kotlin/com/taurin190/testsample/TestsampleApplication.kt
@@ -0,0 +1,11 @@
+package com.taurin190.testsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestsampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestsampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/taurin190/testsample/TestsampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.taurin190.testsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestsampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

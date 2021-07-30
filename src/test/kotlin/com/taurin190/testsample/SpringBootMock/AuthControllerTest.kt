cdaeea7573d97c513390fbfad3504d8adb3fc4a7
Koichi Taura <taura.koichi@gmail.com>
2021-07-30 10:53:49 +0900

    add login test for auth controller with spring boot mock

--- src/test/kotlin/com/taurin190/testsample/SpringBootMock/AuthControllerTest.kt
+++ src/test/kotlin/com/taurin190/testsample/SpringBootMock/AuthControllerTest.kt
@@ -1,10 +1,17 @@
 package com.taurin190.testsample.SpringBootMock
 
+import com.ninjasquad.springmockk.MockkBean
+import com.taurin190.testsample.AuthService
+import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -18,6 +25,12 @@ class AuthControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
+
+    @MockkBean
+    private lateinit var authService: AuthService
+
     @Test
     fun testShowAuthLogin() {
         mockMvc.perform(
@@ -26,5 +39,25 @@ class AuthControllerTest {
             .andExpect(MockMvcResultMatchers.status().is2xxSuccessful)
             .andExpect(MockMvcResultMatchers.model()
                 .attributeExists("loginForm"))
+    }
+
+    @Test
+    fun testPostAuthLogin() {
+        every {
+            authService.loadUserByUsername(any())
+        } returns User.withUsername("test")
+            .password(passwordEncoder.encode("test"))
+            .roles("ADMIN")
+            .build()
+
+        mockMvc.perform(
+            SecurityMockMvcRequestBuilders.formLogin()
+                .loginProcessingUrl("/auth/login")
+                .user("test")
+                .password("test"))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().is3xxRedirection)
+            .andExpect(MockMvcResultMatchers.redirectedUrl("/"))
+            .andExpect(SecurityMockMvcResultMatchers.authenticated().withUsername("test"))
     }
 }

b44200193392b04116d7cfa49c51ae5a5744b240
Koichi Taura <taura.koichi@gmail.com>
2021-07-30 10:53:08 +0900

    return encoded password with auth service

--- src/main/kotlin/com/taurin190/testsample/AuthService.kt
+++ src/main/kotlin/com/taurin190/testsample/AuthService.kt
@@ -1,15 +1,20 @@
 package com.taurin190.testsample
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Service
 class AuthService: UserDetailsService {
+    @Autowired
+    lateinit var passwordEncoder: PasswordEncoder
+
     override fun loadUserByUsername(username: String?): UserDetails {
         return User.withUsername("test")
-            .password("test")
+            .password(passwordEncoder.encode("test"))
             .roles("ADMIN")
             .build()
     }
